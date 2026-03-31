de4983e9d495834b750eda3d81bc77a76360231d
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-03-31 16:14:40 -0500

    Feat: Creación de la entidad categoria

--- src/main/java/com/syncra/supermarket/Entity/CategorieEntity.java
+++ src/main/java/com/syncra/supermarket/Entity/CategorieEntity.java
@@ -1,6 +1,23 @@
 package com.syncra.supermarket.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@Table(name = "categories")
 public class CategorieEntity {
 
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+
+@Column(name = "name")
+private String name;
 
 }
