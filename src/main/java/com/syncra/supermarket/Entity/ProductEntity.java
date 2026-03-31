b8f34abb2490a31d079f12ab059679a959119051
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-03-31 16:25:00 -0500

    Feat: Creacion de la entidad producto

--- src/main/java/com/syncra/supermarket/Entity/ProductEntity.java
+++ src/main/java/com/syncra/supermarket/Entity/ProductEntity.java
@@ -1,5 +1,39 @@
 package com.syncra.supermarket.Entity;
 
+
+import java.util.Locale.Category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
 public class ProductEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "barcode")
+    private String barcode;
+    @Column(name = "price")
+    private double price;
+    @Column(name = "stock")
+    private int stock;
+    @Column(name = "state")
+    private boolean state;
+
+    @ManyToOne
+    @JoinColumn(name = "categories_id")
+    private Category category;
+
 }
