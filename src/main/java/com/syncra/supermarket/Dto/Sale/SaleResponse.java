bd214197a3c2c16c13cb990a8af4f0e9a4c3773d
Jazmín <nathaliaocampo2007@gmail.com>
2026-03-31 20:44:19 -0500

    feat: Se hizo el Sale

--- src/main/java/com/syncra/supermarket/Dto/Sale/SaleResponse.java
+++ src/main/java/com/syncra/supermarket/Dto/Sale/SaleResponse.java
@@ -1,5 +1,19 @@
 package com.syncra.supermarket.Dto.Sale;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class SaleResponse {
     
+private Long saleId;
+
 }
