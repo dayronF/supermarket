7e51f9a823799a7d78f61071d778bd554b50903b
dayronF <dayronflorez0@gmail.com>
2026-04-01 16:40:59 -0500

    feat: creacion de categoryRepository

--- src/main/java/com/syncra/supermarket/Repository/CategoryRepository.java
+++ src/main/java/com/syncra/supermarket/Repository/CategoryRepository.java
@@ -1,5 +1,12 @@
 package com.syncra.supermarket.Repository;
 
-public class CategoryRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.syncra.supermarket.Entity.CategorieEntity;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+
+
+} 
