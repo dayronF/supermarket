3ea20de383daae36a56b9cf9ce67be775c27d76f
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-04-01 21:49:15 -0500

    Fear: Creacion de la interface/Repositorio producto

--- src/main/java/com/syncra/supermarket/Repository/EmployeeRepository.java
+++ src/main/java/com/syncra/supermarket/Repository/EmployeeRepository.java
@@ -3,11 +3,13 @@ package com.syncra.supermarket.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.syncra.supermarket.Entity.EmployeeEntity;
 import java.time.LocalDate;
 
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Integer>{
 
 
