0567962a4b8c4935513b3405f9983b5bee8f5202
dayronF <dayronflorez0@gmail.com>
2026-04-01 20:37:25 -0500

    feat: Creacion de registrar categorias

--- src/main/java/com/syncra/supermarket/Service/CategoryService.java
+++ src/main/java/com/syncra/supermarket/Service/CategoryService.java
@@ -1,5 +1,21 @@
 package com.syncra.supermarket.Service;
 
+
+
+import org.springframework.stereotype.Service;
+
+import com.syncra.supermarket.Entity.CategoryEntity;
+import com.syncra.supermarket.Repository.CategoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
-    
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryEntity save(CategoryEntity category) {
+        return categoryRepository.save(category);
+    }
 }
