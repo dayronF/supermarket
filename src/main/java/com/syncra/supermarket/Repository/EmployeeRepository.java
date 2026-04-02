6f9a9f2699fc57e144dd58d6418e74f2f0375bb0
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-04-01 21:40:21 -0500

    Feat: Creación de la interface/repositorio Empleado

--- src/main/java/com/syncra/supermarket/Repository/EmployeeRepository.java
+++ src/main/java/com/syncra/supermarket/Repository/EmployeeRepository.java
@@ -1,5 +1,18 @@
 package com.syncra.supermarket.Repository;
 
-public class EmployeeRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.syncra.supermarket.Entity.EmployeeEntity;
+import java.time.LocalDate;
+
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Integer>{
+
+
+List<EmployeeEntity> findByPost(EmployeeEntity.Post post);
+
+List<EmployeeEntity> findByEntrydate(LocalDate StarDate , LocalDate EndDate);
+
 }
