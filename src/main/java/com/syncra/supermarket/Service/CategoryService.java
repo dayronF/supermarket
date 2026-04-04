da8860a65c2d0d887bc03bec2d99963e062b8418
dayronF <dayronflorez0@gmail.com>
2026-04-03 19:24:15 -0500

    fix: Correccion de despliegue de productos por categoria

--- src/main/java/com/syncra/supermarket/Service/CategoryService.java
+++ src/main/java/com/syncra/supermarket/Service/CategoryService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.syncra.supermarket.Dto.Category.CategoryMessage;
 import com.syncra.supermarket.Dto.Category.CategoryRequest;
 import com.syncra.supermarket.Dto.Category.CategoryResponse;
+import com.syncra.supermarket.Dto.Product.ProductResponse;
 import com.syncra.supermarket.Entity.CategoryEntity;
 import com.syncra.supermarket.Repository.CategoryRepository;
 
@@ -19,6 +20,8 @@ import lombok.RequiredArgsConstructor;
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    
+
 
     public CategoryMessage createCategory(CategoryRequest request) {
 
@@ -35,6 +38,7 @@ public class CategoryService {
     }
 
     public List<CategoryResponse> listCategory() {
+
         return categoryRepository.findAll().stream()
                 .map(category -> {
                     CategoryResponse response = new CategoryResponse();
@@ -48,11 +52,22 @@ public class CategoryService {
     public CategoryResponse searchId(int id) {
         Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
         if (categoryOptional.isEmpty()) {
-            return null;
+         return null;
         }
         CategoryResponse response = new CategoryResponse();
         response.setId(categoryOptional.get().getId());
         response.setName(categoryOptional.get().getName());
+        response.setProducts(categoryOptional.get().getProducts().stream()
+                .map(product -> {
+                    ProductResponse productResponse = new ProductResponse();
+                    productResponse.setId(product.getId());
+                    productResponse.setName(product.getName());
+                    productResponse.setBarcode(product.getBarcode());
+                    productResponse.setPrice(product.getPrice());
+                    productResponse.setStock(product.getStock());
+                    return productResponse;
+                })
+                .collect(Collectors.toList()));
         return response;
     }
 
