43016bc71a1953671c8d7c0386e509449be93e12
iRay1h <andresf.echeverric08@gmail.com>
2026-04-06 23:17:01 -0500

    Fix: Correcion de bugs

--- src/main/java/com/syncra/supermarket/Repository/EmployeeRepository.java
+++ src/main/java/com/syncra/supermarket/Repository/EmployeeRepository.java
@@ -1,21 +1,20 @@
 package com.syncra.supermarket.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.syncra.supermarket.Entity.EmployeeEntity;
-import java.time.LocalDate;
-
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Integer>{
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
 
-boolean existByCc (int cc);
+    boolean existsByCc(int cc);
 
-List<EmployeeEntity> findByPost(EmployeeEntity.Post post);
+    List<EmployeeEntity> findByPost(EmployeeEntity.Post post);
 
-List<EmployeeEntity> findByEntryDateBetween(LocalDate StarDate , LocalDate EndDate);
+    List<EmployeeEntity> findByEntrydateBetween(LocalDate startDate, LocalDate endDate);
 
 }
